3ce6fc87e7391e2297dea5bc0dae1466adeb5876
Emanuel Alvares Lima Camargo Ribeiro <59899135+Emanuel-Ribeiro@users.noreply.github.com>
2026-08-08 12:47:48 -0300

    theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,1 +1,28 @@
-/home/toor/.config/omarchy/current/theme/neovim.lua
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      terminal_colors = true,
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      contrast = "medium",
+      dim_inactive = false,
+      transparent_mode = false,
+    },
+    config = function(_, opts)
+      require("gruvbox").setup(opts)
+
+      vim.o.background = "dark"
+      vim.cmd.colorscheme("gruvbox")
+    end,
+  },
+}
